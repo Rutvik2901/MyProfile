cfaae4f46f0d708251cc0bd28edbd0ef89660875
Rutvik2901 <gajjarrutvik291@gmail.com>
2021-05-17 21:02:40 +0530

    Added experience

--- personal-website/src/Component/SecondComponent/index.tsx
+++ personal-website/src/Component/SecondComponent/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Chip, Link, makeStyles, MobileStepper, Paper, Theme, Typography, useMediaQuery, useTheme, WithStyles, withStyles } from "@material-ui/core";
+import { AppBar, Box, Button, Card, Chip, Link, makeStyles, MobileStepper, Paper, Tab, Tabs, Theme, Typography, useMediaQuery, useTheme, WithStyles, withStyles } from "@material-ui/core";
 import { KeyboardArrowLeft, KeyboardArrowRight } from "@material-ui/icons";
 import React, { Component } from "react";
 import ForwardIcon from "@material-ui/icons/Forward";
@@ -7,6 +7,8 @@ import GitHubIcon from "@material-ui/icons/GitHub";
 import EmailIcon from "@material-ui/icons/Email";
 import InstagramIcon from "@material-ui/icons/Instagram";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import DoubleArrowIcon from "@material-ui/icons/DoubleArrow";
+
 import {
   JsIcon,
   TsIcon,
@@ -105,27 +107,13 @@ const styles = (theme: Theme) => ({
         width: "90%",
       },
     },
-    // opacity: 0,
-    // animation: `$animated-text 2s`,
   },
   "@keyframes animated-text": {
     from: { opacity: 0 },
     to: { opacity: 1 },
   },
-  "@keyframes skills-text": {
-    from: { opacity: 0 },
-    to: { opacity: 1 },
-    // from: { transform: "translateX(-50px)" },
-    // to: { transform: "translateX(0px)" },
-  },
-  "@keyframes skills-text1": {
-    from: { opacity: 0 },
-    to: { opacity: 1 },
-    // from: { transform: "translateX(50px)" },
-    // to: { transform: "translateX(0px)" },
-  },
   gridSystem: {
-    animation: "$skills-text 2s",
+    animation: "$animated-text 2s",
     display: "grid",
     gridTemplateColumns: "25% 25% 25% 25%",
     [theme.breakpoints.down("sm")]: {
@@ -166,12 +154,43 @@ const styles = (theme: Theme) => ({
       height: "1.5em",
     },
   },
+  rootExperience: {
+    flexGrow: 1,
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+    // height: "100%",
+    background: "transparent",
+    fontSize: 14,
+    "& .MuiPaper-elevation10": {
+      marginTop: 15,
+      width: "70%",
+      [theme.breakpoints.down("sm")]: {
+        width: "80%",
+      },
+      [theme.breakpoints.down("xs")]: {
+        width: "90%",
+      },
+    },
+  },
+  labels: {
+    "& .MuiTab-textColorPrimary": {
+      color: "rgba(255, 255, 255, 0.5)",
+      fontSize: "1em",
+    },
+    "& .MuiTab-textColorPrimary.Mui-selected": {
+      color: "#3f51b5",
+      fontSize: "1em",
+      fontWeight: 600,
+    },
+  },
 });
 
 interface ISecondComponentProps extends WithStyles<typeof styles> {}
 
 interface ISecondComponentState {
   activeStep: number;
+  value: number;
 }
 class SecondComponent extends Component<ISecondComponentProps, ISecondComponentState> {
   constructor(props: ISecondComponentProps) {
@@ -179,6 +198,7 @@ class SecondComponent extends Component<ISecondComponentProps, ISecondComponentS
 
     this.state = {
       activeStep: 0,
+      value: 0,
     };
   }
 
@@ -331,6 +351,37 @@ class SecondComponent extends Component<ISecondComponentProps, ISecondComponentS
     );
   };
 
+  private handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    this.setState({
+      value: newValue,
+    });
+  };
+
+  private experienceComponent = () => {
+    const { classes } = this.props;
+    const { value } = this.state;
+    return (
+      <div className={classes.rootExperience} style={{ overflowY: "auto" }}>
+        <Paper elevation={10} square style={{ backgroundColor: "transparent", overflowY: "auto" }}>
+          <AppBar position="static" style={{ background: "transparent" }}>
+            <Tabs className={classes.labels} value={value} onChange={this.handleChange} indicatorColor="primary" textColor="primary" variant="fullWidth" aria-label="scrollable auto tabs example">
+              <Tab className={classes.labels} label="Internships" />
+              <Tab className={classes.labels} label="Projects" />
+            </Tabs>
+          </AppBar>
+          {/* <div style={{ overflowY: "auto" }}> */}
+          <TabPanel value={value} index={0}>
+            {/* Item One */}
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            {/* Item Two */}
+          </TabPanel>
+        </Paper>
+        {/* </div> */}
+      </div>
+    );
+  };
+
   private renderComponent = () => {
     const { activeStep } = this.state;
 
@@ -340,6 +391,8 @@ class SecondComponent extends Component<ISecondComponentProps, ISecondComponentS
       case 1:
         return this.skillsComponent();
       case 2:
+        return this.experienceComponent();
+      case 3:
         return this.contactComponent();
       default:
         break;
@@ -348,8 +401,8 @@ class SecondComponent extends Component<ISecondComponentProps, ISecondComponentS
 
   render() {
     const { classes } = this.props;
-    const maxSteps = 3;
-    const title = ["Welcome", "Skills", "Thank You"];
+    const maxSteps = 4;
+    const title = ["Welcome", "Skills", "Experience", "Thank You"];
 
     return (
       <div style={{ flexDirection: "column", height: "100vh", fontSize: "5vh" }} className={classes.root}>
@@ -394,3 +447,176 @@ class SecondComponent extends Component<ISecondComponentProps, ISecondComponentS
 }
 
 export default withStyles(styles)(SecondComponent);
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: any;
+  value?: any;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div role="tabpanel" hidden={value !== index} id={`vertical-tabpanel-${index}`} aria-labelledby={`vertical-tab-${index}`} {...other}>
+      <Experience index={index} />
+    </div>
+  );
+}
+
+function Experience(props: TabPanelProps) {
+  const contentAjmera = [
+    "Ajmera Infotech is Full-cycle programming development specialist organization for mobile and web apps based in India.",
+    "Learning of new tools like Atlassian Jira, Atlassian Confluence, Git and hands-on experience of react",
+    "Involved in development of High-end and innovative products using different tools and technologies.",
+    "Convert figma rough wireframes into high-end, responsive web designs.",
+  ];
+  const contentTiny = [
+    "Involved in developing website using Django a python framework.",
+    "Data Analytics using machine learning algorithms.",
+    "Analyze Covid-19 day to day data and also predict future data.",
+  ];
+
+  const contentDigi = [
+    "Utilized Object oriented concepts for the business logic and developed MVC application for the client.",
+    "Worked with a verity of different languages, framework, and tools such as Python, Tomcat Apache, Angular, MongoDB.",
+    "Designed and implemented a new web service to handle specific client’s business requests.",
+  ];
+  const contentRobo = [
+    "Runners up in robot competition of 8 different types of categories.",
+    "Object detection and tracking with advance machine learning algorithms.",
+    "Live camera and sensors attached with robot helps to gather surroudings infomation and send back to ground station at regular interval.",
+  ];
+  const contentQuiz = [
+    "Online Examination portal for organizations to take exam's",
+    "Frontend Developed in Angular, HTML, CSS, Bootstrap and backend developed in Node Js, Express and database used is MongoDB.",
+    "Admin Portal makes easy for admin's to manage organizations very effectively.",
+  ];
+  const techStackAjmera = ["React", "Typescript", "React-redux", "Redex-saga", "Figma"];
+  const techStackTiny = ["Python", "Django", "HTML5", "CSS3", "Javascript", "PostgreSQL"];
+  const techStackDigi = ["Angular", "Python", "MongoDB", "HTML5", "CSS3", "JQuery"];
+  const techStackRobo = ["Computer Vision", "Python", "Django", "HTML5", "CSS3", "Javascript", "RestAPI", "PostgreSQL", "AJAX"];
+  const techStackQuiz = ["Angular", "Node.Js", "Bootstrap", "RestAPI", "Express", "MongoDB", "Heroku"];
+  const contentMain = [contentAjmera, contentTiny, contentDigi, contentRobo, contentQuiz];
+  const techStackMain = [techStackAjmera, techStackTiny, techStackDigi, techStackRobo, techStackQuiz];
+  const teamLengthMain = [0, 0, 0, 5, 3];
+  const positionMain = ["Software Developer Intern", "Software Developer Intern", "Software Developer Intern", "", ""];
+  const durationMain = ["Jan'21 to Apr'21", "May'20 to Jul'20", "Jan'20 to May'20", "", ""];
+  const locationMain = ["Gujarat, India", "Gujarat, India", "Gujarat, India", "", ""];
+  const titleMain = ["Ajmera Infotech Pvt. Ltd.", "Tiny ERP Pvt. Ltd - Odoo", "Digiwagon Technologies", "ROBOFEST Competition", "Quiz Generator"];
+  const maxSteps = titleMain.length;
+  const { index } = props;
+
+  if (index) {
+    return (
+      <div style={{ backgroundColor: "transparent", padding: "10px" }}>
+        <ExperienceLayout
+          position={positionMain[3]}
+          duration={durationMain[3]}
+          techStack={techStackMain[3]}
+          content={contentMain[3]}
+          location={locationMain[3]}
+          title={titleMain[3]}
+          teamLength={teamLengthMain[3]}
+        />
+        <ExperienceLayout
+          position={positionMain[4]}
+          duration={durationMain[4]}
+          techStack={techStackMain[4]}
+          content={contentMain[4]}
+          location={locationMain[4]}
+          title={titleMain[4]}
+          teamLength={teamLengthMain[4]}
+        />
+      </div>
+    );
+  } else {
+    return (
+      <div style={{ backgroundColor: "transparent", padding: "10px" }}>
+        <ExperienceLayout
+          position={positionMain[0]}
+          duration={durationMain[0]}
+          techStack={techStackMain[0]}
+          content={contentMain[0]}
+          location={locationMain[0]}
+          title={titleMain[0]}
+          teamLength={teamLengthMain[0]}
+        />
+        <ExperienceLayout
+          position={positionMain[1]}
+          duration={durationMain[1]}
+          techStack={techStackMain[1]}
+          content={contentMain[1]}
+          location={locationMain[1]}
+          title={titleMain[1]}
+          teamLength={teamLengthMain[1]}
+        />
+        <ExperienceLayout
+          position={positionMain[2]}
+          duration={durationMain[2]}
+          techStack={techStackMain[2]}
+          content={contentMain[2]}
+          location={locationMain[2]}
+          title={titleMain[2]}
+          teamLength={teamLengthMain[2]}
+        />
+      </div>
+    );
+  }
+}
+
+const useStyles = makeStyles((theme: Theme) => ({
+  "@global": {
+    "@keyframes animated-text": {
+      from: { opacity: 0 },
+      to: { opacity: 1 },
+    },
+  },
+  root: {
+    animation: "$animated-text 2s",
+  },
+}));
+
+const ExperienceLayout = (props: any) => {
+  const classes = useStyles();
+
+  return (
+    <Paper className={classes.root} style={{ color: "rgb(108, 117, 125)", background: "transparent", marginTop: 10, padding: 10 }} square elevation={0}>
+      <Typography style={{ fontWeight: 600, fontSize: "1.2rem" }}>{props.title}</Typography>
+      {props.position !== "" && (
+        <Typography style={{ marginTop: 5 }} variant="body2" component="p">
+          Position: {props.position}
+        </Typography>
+      )}
+      {props.duration !== "" && (
+        <Typography variant="body2" component="p">
+          Duration: {props.duration}
+        </Typography>
+      )}
+      {props.location !== "" && (
+        <Typography variant="body2" component="p">
+          Location: {props.location}
+        </Typography>
+      )}
+
+      {props.teamLength !== 0 && (
+        <Typography variant="body2" component="p">
+          Team: {props.teamLength} Members
+        </Typography>
+      )}
+
+      <Typography style={{ display: "flex", flexDirection: "column", marginTop: 5 }} gutterBottom variant="h5" component="h4">
+        {props.content.map((signleContent: string) => (
+          <div style={{ marginTop: 5, display: "flex" }}>
+            <DoubleArrowIcon />
+            <Typography style={{ display: "flex", flexWrap: "wrap" }}>{signleContent}</Typography>
+          </div>
+        ))}
+      </Typography>
+      {props.techStack.map((tech: any) => (
+        <Chip label={tech} size="small" variant="default" style={{ color: "rgba(108, 117, 125)", backgroundColor: "transparent", border: "1px solid", margin: 5 }}></Chip>
+      ))}
+      {/* className={styles.chipStyles} */}
+    </Paper>
+  );
+};
