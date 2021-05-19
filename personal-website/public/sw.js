b6e485320677b5d912152fed1b245bd6b7721ee2
Rutvik2901 <gajjarrutvik291@gmail.com>
2021-05-19 23:13:54 +0530

    offline supprt

--- personal-website/public/sw.js
+++ personal-website/public/sw.js
@@ -1,23 +1,21 @@
 
 self.addEventListener('install',(evt) => {
-    evt.waitUntil(
-        caches.open('cache-static').then(cache => {
-        cache.addAll(['/', 'static/js/main.350f67b3.chunk.js', 'static/js/2.59d9be5b.chunk.js' ,'favicon.ico','/static/js/bundle.js','/static/js/vendors~main.chunk.js','/static/js/main.chunk.js','/logo.png','/manifest.json','https://fonts.googleapis.com/css2?family=Pangolin&family=Source+Serif+Pro:wght@300&family=Roboto:wght@300&family=Cookie&family=Katibeh&family=Noto+Serif&family=VT323&display=swap'])
-    }))
 })
 
 self.addEventListener('activate', () => {
 })
 
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', (event) => {
 
-    e.respondWith(caches.match(e.request.url).then(cacheResponse => {
-        return cacheResponse || fetch(e.request)
+    
+    event.respondWith(
+        caches.open('cache-static').then(cache => {
+            return cache.match(event.request).then(response => {
+                return response || fetch(event.request).then(response => {
+                  cache.put(event.request, response.clone());
+                  return response;
+                });
+              });
+
     }))
 })
-
-// self.addEventListener('beforeinstallprompt', (e) => {
-//     e.preventDefault();
-//     showInstallPromotion();
-//     // btnAdd.style.display = 'block';
-// })
